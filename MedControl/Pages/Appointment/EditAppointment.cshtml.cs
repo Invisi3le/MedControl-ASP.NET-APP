c00627e180a201eaeb5cb14b395698d3e02f1047
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-05-16 12:19:06 +0200

    new theme

---
+++ MedControl/Pages/Appointment/EditAppointment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MedControl.Pages.Appointment
+{
+    public class EditAppointmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
