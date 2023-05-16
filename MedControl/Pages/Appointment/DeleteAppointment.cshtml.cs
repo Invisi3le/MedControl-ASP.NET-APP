d526c4fe1d3aa8948fab7ab164322fff034ff41d
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-05-16 12:23:31 +0200

    create delete and create files

---
+++ MedControl/Pages/Appointment/DeleteAppointment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MedControl.Pages.Appointment
+{
+    public class DeleteAppointmentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
