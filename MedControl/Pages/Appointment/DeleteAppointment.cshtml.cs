1369a844ffb986b1233c075cb2219747060319bd
Monika Szewczyk <monika.szewczyk120@gmail.com>
2023-05-16 13:04:08 +0200

    add delete page

--- MedControl/Pages/Appointment/DeleteAppointment.cshtml.cs
+++ MedControl/Pages/Appointment/DeleteAppointment.cshtml.cs
@@ -1,3 +1,5 @@
+using MedControl.Data;
+using MedControl.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,32 @@ namespace MedControl.Pages.Appointment
 {
     public class DeleteAppointmentModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        
+        public AppointmentModel Appointment { get; set; }
+
+        public DeleteAppointmentModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            Appointment = _db.Appointments.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var appointmentFromDb = _db.Appointments.Find(Appointment.Id);
+            if (appointmentFromDb != null)
+            {
+                _db.Appointments.Remove(appointmentFromDb);
+                await _db.SaveChangesAsync();
+                TempData["success"] = "Appointment deleted successfully";
+                return RedirectToPage("AppointmentsList");
+            }
+
+
+            return Page();
         }
     }
 }
